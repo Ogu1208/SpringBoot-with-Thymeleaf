1424e43c47fbcc6e448c80c2179b10afd1134ae7
Ogu1208 <kdasunb@naver.com>
2023-05-02 23:05:04 +0900

    first commit

---
+++ src/main/java/com/example/mythemeleaf/MythemeleafApplication.java
@@ -0,0 +1,13 @@
+package com.example.mythemeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MythemeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MythemeleafApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mythemeleaf/MythemeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mythemeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MythemeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
